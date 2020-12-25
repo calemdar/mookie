51fba0bf817aaf695b7e6e2baae96775953e68aa
Ertugrul Alemdar <ertalemdar@gmail.com>
2020-12-24 19:41:11 -0500

    change templates

--- mookie-api/src/Server.ts
+++ mookie-api/src/Server.ts
@@ -1,63 +1,25 @@
-import cookieParser from 'cookie-parser';
-import morgan from 'morgan';
-import path from 'path';
-import helmet from 'helmet';
-
-import express, { NextFunction, Request, Response } from 'express';
-import StatusCodes from 'http-status-codes';
-import 'express-async-errors';
-
-import BaseRouter from './routes';
-import logger from '@shared/Logger';
-
-const app = express();
-const { BAD_REQUEST } = StatusCodes;
+import errorHandler from "errorhandler";
+import app from "./app";
 
 
-
-/************************************************************************************
- *                              Set basic express settings
- ***********************************************************************************/
-
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(cookieParser());
-
-// Show routes called in console during development
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+/**
+ * Error Handler. Provides full stack
+ */
+if (process.env.NODE_ENV === "development") {
+    app.use(errorHandler());
 }
 
-// Security
-if (process.env.NODE_ENV === 'production') {
-    app.use(helmet());
-}
 
-// Add APIs
-app.use('/api', BaseRouter);
-
-// Print API errors
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    logger.err(err, true);
-    return res.status(BAD_REQUEST).json({
-        error: err.message,
-    });
+/**
+ * Start Express server.
+ */
+const server = app.listen(app.get("port"), () => {
+    console.log(
+        "  App is running at http://localhost:%d in %s mode",
+        app.get("port"),
+        app.get("env")
+    );
+    console.log("  Press CTRL-C to stop\n");
 });
 
-
-
-/************************************************************************************
- *                              Serve front-end content
- ***********************************************************************************/
-
-const viewsDir = path.join(__dirname, 'views');
-app.set('views', viewsDir);
-const staticDir = path.join(__dirname, 'public');
-app.use(express.static(staticDir));
-app.get('*', (req: Request, res: Response) => {
-    res.sendFile('index.html', {root: viewsDir});
-});
-
-// Export express instance
-export default app;
+export default server;
